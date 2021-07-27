f19dd9ea0ab88599b3265f700509f9d4417df31b
Guga <contato.gustavobarbosa@outlook.com>
2021-07-27 18:12:51 -0300

    Preparando revisão do código do Fernando

--- src/schemas/bodySchema.ts
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const bodySchema = Joi.object({
-  name: Joi.string().trim().required(),
-  youtubeLink: Joi.string().required(),
-});
--- tests/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import connection from "../../src/database";
-
-export async function endConnection() {
-  await connection.end();
-}
-
-export async function clearDatabase() {
-  await connection.query(`TRUNCATE "music_list" RESTART IDENTITY`);
-}
